12e6b2d76583332fd7e73c56c081a0f35d706d26
Tadas V <tadovas@gmail.com>
2018-08-24 16:24:41 +0300

    All major oses should work now

--- openvpn/client_factory.go
+++ openvpn/client_factory.go
@@ -1,5 +1,3 @@
-// +build !darwin
-
 /*
  * Copyright (C) 2017 The "MysteriumNetwork/node" Authors.
  *
@@ -19,14 +17,89 @@
 
 package openvpn
 
-// NewClient creates openvpn client with given config params
-func NewClient(openvpnBinary string, config *ClientConfig, stateHandler state.Callback, statsHandler bytescount.SessionStatsHandler, credentialsProvider auth.CredentialsProvider) Process {
+import (
+	"github.com/MysteriumNetwork/openvpn3-go-bindings/openvpn3"
+	log "github.com/cihub/seelog"
+	"strings"
+)
 
-	return CreateNewProcess(
-		openvpnBinary,
-		config.GenericConfig,
-		state.NewMiddleware(stateCallback),
-		bytescount.NewMiddleware(statsHandler, 1*time.Second),
-		auth.NewMiddleware(credentialsProvider),
-	)
+const openvpn3SessionPrefx = "[openvpn3 session] "
+
+// NewClient creates openvpn client with given config params
+func NewClient(openvpnBinary string, config *ClientConfig, stateHandler StateCallback, statsHandler SessionStatsHandler, credentialsProvider CredentialsProvider) Process {
+	return &openvpn3Session{
+		ovpn3: openvpn3.NewSession(&openvpn3Callbacks{
+			stateCallback: stateHandler,
+			statsHandler:  statsHandler,
+		}),
+		config:        config,
+		credsProvider: credentialsProvider,
+		stateCallback: stateHandler,
+	}
 }
+
+type openvpn3Session struct {
+	ovpn3         *openvpn3.Session
+	config        *ClientConfig
+	credsProvider CredentialsProvider
+	stateCallback StateCallback
+}
+
+func (session *openvpn3Session) Start() error {
+	profile, err := session.config.ToConfigFileContent()
+	if err != nil {
+		return err
+	}
+	log.Info(openvpn3SessionPrefx, "Using client profile")
+	log.Info(openvpn3SessionPrefx, profile)
+	credentials := openvpn3.Credentials{}
+	credentials.Username, credentials.Password, err = session.credsProvider()
+	if err != nil {
+		return err
+	}
+
+	session.ovpn3.Start(profile, credentials)
+	return nil
+}
+
+func (session *openvpn3Session) Wait() error {
+	defer session.stateCallback(ProcessExited)
+	return session.ovpn3.Wait()
+}
+
+func (session *openvpn3Session) Stop() {
+	session.ovpn3.Stop()
+}
+
+var _ Process = &openvpn3Session{}
+
+type openvpn3Callbacks struct {
+	stateCallback StateCallback
+	statsHandler  SessionStatsHandler
+}
+
+func (callbacks *openvpn3Callbacks) Log(text string) {
+	lines := strings.Split(text, "\n")
+	for _, line := range lines {
+		log.Info(openvpn3SessionPrefx, line)
+	}
+}
+
+func (callbacks *openvpn3Callbacks) OnEvent(event openvpn3.Event) {
+	log.Infof("%s%+v\n", openvpn3SessionPrefx, event)
+	callbacks.stateCallback(State(event.Name))
+}
+
+func (callbacks *openvpn3Callbacks) OnStats(statistics openvpn3.Statistics) {
+	stats := SessionStats{
+		BytesSent:     statistics.BytesOut,
+		BytesReceived: statistics.BytesIn,
+	}
+	callbacks.statsHandler(stats)
+}
+
+var _ openvpn3.Logger = &openvpn3Callbacks{}
+
+var _ openvpn3.EventConsumer = &openvpn3Callbacks{}
+
+var _ openvpn3.StatsConsumer = &openvpn3Callbacks{}
